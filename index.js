4562b65b578c81ac243ec967f31abb79131f38b0
Hans Kool <Hansjekool@hotmail.com>
2020-10-05 14:30:53 +0200

    rest spread

--- index.js
+++ index.js
@@ -112,4 +112,25 @@ makeAllCaps(complicatedArray)
   .then((result) => console.log(result))
   .catch((error) => console.log(error));
 
+// rest spread
+
+console.log("rest/spread")
+
+const optellen = function(...args) {
+  return args.reduce((acc,item) => {
+          return acc + item;
+  })
+}
+
+console.log(optellen(1,2,3,4,5,6)); // verwachte uitkomst: 21
+
+const optellen2 = function(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+const cijfers = [1, 2, 3];
+
+console.log(optellen2(...cijfers));
+// verwachte uitkomst: 6
+
 console.log("The End");
